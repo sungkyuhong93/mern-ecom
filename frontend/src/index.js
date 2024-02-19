f6ab10bf250d23dded044769e1d6ada5f0e64d86
Sung Kyu Hong <sungkyuhong93@gmail.com>
2024-02-19 16:38:28 -0500

    add backend auth logic

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,15 @@ import {
   Route,
   RouterProvider
 } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './store';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import App from './App';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 import reportWebVitals from './reportWebVitals';
 
 const router = createBrowserRouter(
@@ -19,6 +22,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/products/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
     </Route>
   )
 )
@@ -26,7 +30,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,27 +1,37 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useState } from 'react';
+import { useParams, useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import { Row, Col, Image, ListGroup, Card, Button, Form } from "react-bootstrap";
+import { useDispatch } from "react-redux";
 import Rating from "../components/Rating";
-import axios from "axios";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
+import { addToCart } from "../slices/cartSlice";
 
 const ProductScreen = () => {
-  const [product, setProduct] = useState({});
   const {id: productId} = useParams();
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${productId}`);
-      setProduct(data);
-    }
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    fetchProduct();
-  }, [productId])
+  const [qty, setQty] = useState(1);
+
+  const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
+
+  const addToCartHandler = () => {
+    dispatch(addToCart({ ...product, qty }));
+    navigate('/cart');
+  }
 
   return (
     <>
       <Link to="/" className="btn btn-light my-3">Go Back</Link>
-      <Row>
+
+      { isLoading ? (
+       <Loader />
+      ) : error ? (<Message variant="danger">{error?.data?.message || error.error}</Message>) : (
+        <Row>
         <Col md={5}>
           <Image src={product.image} alt={product.name} fluid />
         </Col>
@@ -61,8 +71,34 @@ const ProductScreen = () => {
                 </Row>
               </ListGroup.Item>
 
+              {product.countInStock > 0 && (
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Qty</Col>
+                    <Col>
+                      <Form.Control
+                        as="select"
+                        value={qty}
+                        onChange={(e) => setQty(Number(e.target.value))}
+                      >
+                        {[...Array(product.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </Form.Control>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              )}
+
               <ListGroup.Item>
-                <Button className="btn-block" type="button" disabled={product.countInStock === 0}>
+                <Button 
+                  className="btn-block" 
+                  type="button" 
+                  disabled={product.countInStock === 0}
+                  onClick={addToCartHandler}
+                >
                   Add To Cart
                 </Button>
               </ListGroup.Item>
@@ -70,6 +106,8 @@ const ProductScreen = () => {
           </Card>
         </Col>
       </Row>
+      )}
+
     </>
   )
 }
