bf767f6e9647cbb9b1115cee7ec43342b544d8a0
Sung Kyu Hong <sungkyuhong93@gmail.com>
2024-02-23 15:27:01 -0500

    add admin user functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,29 @@
+import { useNavigate } from "react-router-dom";
 import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa'; 
 import { LinkContainer } from "react-router-bootstrap";
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from "../slices/authSlice";
 import logo from "../assets/logo.png";
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
   const { userInfo } = useSelector((state) => state.auth);
 
-  const logoutHandler = () => {
-    console.log('logout');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/login');
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return (
@@ -52,7 +66,25 @@ const Header = () => {
                   <FaUser /> Sign In
                 </Nav.Link>
               </LinkContainer>)}
-             
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title='Admin' id='adminmenu'>
+                  <LinkContainer to="/admin/productlist">
+                    <NavDropdown.Item>
+                      Products
+                    </NavDropdown.Item>
+                  </LinkContainer> 
+                  <LinkContainer to="/admin/userlist">
+                    <NavDropdown.Item>
+                      Users
+                    </NavDropdown.Item>
+                  </LinkContainer> 
+                  <LinkContainer to="/admin/orderlist">
+                    <NavDropdown.Item>
+                      Orders
+                    </NavDropdown.Item>
+                  </LinkContainer> 
+                </NavDropdown>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
